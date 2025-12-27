8fa209803a833ca7f54611fe142e75a4a51460fb
nicoarditi <nicoarditi12@gmail.com>
2025-12-27 12:55:34 -0300

    config headers

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { CircleUserRound } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 //TODO: Proteger la pagina para que solo el usuario logueado pueda ver su perfil
 
