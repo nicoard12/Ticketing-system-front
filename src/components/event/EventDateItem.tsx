17eee2ccb52ea6d38992089c3e1a761b24277b62
nicoarditi <nicoarditi12@gmail.com>
2026-01-26 09:03:13 -0300

    manejo fecha de evento vencida

--- src/components/event/EventDateItem.tsx
+++ src/components/event/EventDateItem.tsx
@@ -3,6 +3,8 @@ import { Button } from "../ui/button";
 import { useUsuario } from "@/context/UserContext";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
+import { isPast } from "date-fns";
+import { useEffect, useState } from "react";
 
 type EventDateItemProps = {
   date: EventDate;
@@ -14,17 +16,20 @@ function EventDateItem({ date, eventId, index }: EventDateItemProps) {
   const { user } = useUsuario();
   const { loginWithRedirect } = useAuth0();
   const navigate = useNavigate();
+  const [dateExpired, setDateExpired] = useState(isPast(new Date(date.fecha)));
 
   const buyTicket = () => {
     if (!user) loginWithRedirect();
     else navigate(`/evento/${eventId}/fecha/${index + 1}`);
   };
 
-  const goStaffPage= () =>{
-    navigate(`/evento/${eventId}/fecha/${index + 1}/staff`)
-  }
+  const goStaffPage = () => {
+    navigate(`/evento/${eventId}/fecha/${index + 1}/staff`);
+  };
 
-  if (new Date(date.fecha) < new Date()) return null;
+  useEffect(() => {
+    setDateExpired(isPast(new Date(date.fecha)));
+  }, [date]);
 
   return (
     <div className="flex items-center justify-between w-full border border-2 rounded p-2">
@@ -40,23 +45,28 @@ function EventDateItem({ date, eventId, index }: EventDateItemProps) {
         })}
       </p>
 
-      <div className="flex flex-col sm:flex-row gap-3 items-center ">
-        {Number(date.cantidadEntradas) > 0 ? (
-          <span className="text-sm font-thin">
-            Quedan {date.cantidadEntradas} entradas
-          </span>
-        ) : (
-          <span className="text-sm font-thin text-red-600">Agotadas</span>
-        )}
+      {dateExpired ? (
+        <span className="text-red-800 font-bold">Finalizada</span>
+      ) : (
+        <div className="flex flex-col sm:flex-row gap-3 items-center ">
+          {Number(date.cantidadEntradas) > 0 ? (
+            <span className="text-sm font-thin">
+              Quedan {date.cantidadEntradas} entradas
+            </span>
+          ) : (
+            <span className="text-sm font-thin text-red-600">Agotadas</span>
+          )}
 
-        {((!user || user.rol == "normal") && Number(date.cantidadEntradas) > 0 ) && (
-          <Button onClick={buyTicket}>Comprar</Button>
-        )}
+          {(!user || user.rol == "normal") &&
+            Number(date.cantidadEntradas) > 0 && (
+              <Button onClick={buyTicket}>Comprar</Button>
+            )}
 
-        {user?.rol == "staff" && (
-          <Button onClick={goStaffPage}>Comenzar</Button>
-        )}
-      </div>
+          {user?.rol == "staff" && (
+            <Button onClick={goStaffPage}>Comenzar</Button>
+          )}
+        </div>
+      )}
     </div>
   );
 }
